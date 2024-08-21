972a158e87940f892fdaa21edd122296156ff82e
Jorge Tovar <jorgetovar621@gmail.com>
2024-08-21 09:40:30 -0500

    first commit

--- infra/serverless/functions/GetItemsFunction/main.go
+++ infra/serverless/functions/GetItemsFunction/main.go
@@ -2,46 +2,19 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"fmt"
-	"net/http"
+	"github.com/aws/aws-lambda-go/lambda"
 )
 
 type Response struct {
 	Message string `json:"message"`
 }
 
-func handler(ctx context.Context, request map[string]interface{}) (map[string]interface{}, error) {
-	// Create a simple response message
-	resp := Response{
+func HandleRequest(ctx context.Context) (Response, error) {
+	return Response{
 		Message: "Hello, World!",
-	}
-
-	// Marshal the response into JSON
-	body, err := json.Marshal(resp)
-	if err != nil {
-		return map[string]interface{}{
-			"statusCode": http.StatusInternalServerError,
-			"body":       "Internal Server Error",
-		}, nil
-	}
-
-	// Return the response with status code 200
-	return map[string]interface{}{
-		"statusCode": http.StatusOK,
-		"body":       string(body),
 	}, nil
 }
 
 func main() {
-	// Manually invoke the handler function since we're not using aws-lambda-go
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// Convert the request to a map
-		request := map[string]interface{}{}
-		_, err := handler(context.Background(), request)
-		if err != nil {
-			return
-		}
-	})
-	fmt.Println("Lambda function initialized and ready.")
+	lambda.Start(HandleRequest)
 }
--- infra/serverless/functions/go.mod
+++ infra/serverless/functions/go.mod
@@ -1,3 +1,5 @@
 module GetItemsFunction
 
 go 1.21.1
+
+require github.com/aws/aws-lambda-go v1.47.0
